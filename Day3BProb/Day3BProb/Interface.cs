36d9ca5a1e10761a57117f67301fa918bc5cdddd
Krishanaku <krishnakumar.kk650.kk@gmail.com>
2023-04-30 17:44:09 +0530

    Add : Interface Example

---
+++ Day3BProb/Day3BProb/Interface.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day3BProb
+{
+    public interface IAnimal
+    {
+        void MakeSound();
+        string GetSpecies();
+    }
+
+    public class Dogs : IAnimal
+    {
+        public void MakeSound()
+        {
+            Console.WriteLine("Woof!");
+        }
+
+        public string GetSpecies()
+        {
+            return "Canine";
+        }
+    }
+
+    public class Cat : IAnimal
+    {
+        public void MakeSound()
+        {
+            Console.WriteLine("Meow!");
+        }
+
+        public string GetSpecies()
+        {
+            return "Feline";
+        }
+    }
+}
---
+++ Day3BProb/Day3BProb/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Day3BProb
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //InterFace................
+            IAnimal myDog = new Dogs();
+            IAnimal myCat = new Cat();
+
+            myDog.MakeSound();
+            Console.WriteLine(myDog.GetSpecies());
+
+            myCat.MakeSound();
+            Console.WriteLine(myCat.GetSpecies());
+
+        }
+    }
+}
