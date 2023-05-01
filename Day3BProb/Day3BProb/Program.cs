f92f90cbeb3f18ada373754d0e222930a26e9323
Krishanaku <krishnakumar.kk650.kk@gmail.com>
2023-05-01 11:31:33 +0530

    Add : Constructor type Default, Parameterized, Non parameterized

--- Day3BProb/Day3BProb/Program.cs
+++ Day3BProb/Day3BProb/Program.cs
@@ -4,15 +4,27 @@
     {
         public static void Main(string[] args)
         {
-            //InterFace................
-            IAnimal myDog = new Dogs();
-            IAnimal myCat = new Cat();
+            ////InterFace................
+            //IAnimal myDog = new Dogs();
+            //IAnimal myCat = new Cat();
 
-            myDog.MakeSound();
-            Console.WriteLine(myDog.GetSpecies());
+            //myDog.MakeSound();
+            //Console.WriteLine(myDog.GetSpecies());
 
-            myCat.MakeSound();
-            Console.WriteLine(myCat.GetSpecies());
+            //myCat.MakeSound();
+            //Console.WriteLine(myCat.GetSpecies());
+
+            // default constructor.......................
+            ConstructType DefObj= new ConstructType();
+            Console.WriteLine("Default constructor "+DefObj.num);
+            //Non Parameterized........
+            ConstructType Nonpar= new ConstructType();
+            //Parameterized ...................
+            ConstructType construct = new ConstructType("Mustang", "Red", 1969);
+            ConstructType construct1 = new ConstructType(1001, "Shine");
+            construct1.DisplayConstructor();
+            Console.WriteLine( "Parameterized Constructor :"+construct.color + " " + construct.year + " " + construct.model);
+            Console.ReadKey(); 
 
         }
     }
